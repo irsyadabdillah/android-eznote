cb67db7b838a50852f98781fb27468166a6fe630
Irsyad Abdillah <irsyadabdilah96@gmail.com>
2021-03-16 16:58:57 +0700

    Add bottomsheetdialog

---
+++ app/src/main/java/com/irzstudio/eznote/screen/BottomSheetFragment.kt
@@ -0,0 +1,17 @@
+package com.irzstudio.eznote.screen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.irzstudio.eznote.R
+
+class BottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.bottomsheet_fragment, container, false)
+    }
+}
--- app/src/main/java/com/irzstudio/eznote/screen/MainActivity.kt
+++ app/src/main/java/com/irzstudio/eznote/screen/MainActivity.kt
@@ -7,10 +7,12 @@ import android.widget.Adapter
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.irzstudio.eznote.R
 import com.irzstudio.eznote.adapter.ListNoteAdapter
 import com.irzstudio.eznote.data.Note
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottomsheet_fragment.*
 import kotlinx.android.synthetic.main.empty_screen.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,11 +25,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottomSheetFragment = BottomSheetFragment()
+
+        btn_show.setOnClickListener {
+            bottomSheetFragment.show(supportFragmentManager, "BottomSheetDialog")
+        }
+
+        /*BottomSheetBehavior.from(layout_sheet).apply {
+            peekHeight = 200
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
+        }*/
+
         showDataAdapter()
 
     }
 
     private fun showDataAdapter() {
+
+        /*
         val noteDummy = Note(tittle = "Resep Makanan", content = "In android, TextView is a user")
         val noteDummy2 = Note(tittle = "Catatan Sekoah", content = "In android, TextView is a user")
         val noteDummy3 = Note(tittle = "Daily", content = "In android, TextView is a user")
@@ -41,6 +56,8 @@ class MainActivity : AppCompatActivity() {
         dataNote.add(noteDummy4)
         dataNote.add(noteDummy5)
         dataNote.add(noteDummy6)
+
+         */
 
         if (dataNote.size == 0){
             recyclerview_note.visibility = View.GONE
@@ -57,6 +74,7 @@ class MainActivity : AppCompatActivity() {
         recyclerview_note.layoutManager = LinearLayoutManager(this)
 
     }
+
 
 }
 
